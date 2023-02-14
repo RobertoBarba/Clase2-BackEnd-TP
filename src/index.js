bf3b11eee138368cfcf2c8fd7e28fe4becf53977
RobertoBarba <Roberto Barba>
2023-02-13 22:54:48 -0300

    clase2 - tp

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 3000;
+app.use(express.static(__dirname+ '/public'))
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+app.get('/productos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productos.html'))
+})
+
+app.get('/productos1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productos1.html'))
+})
